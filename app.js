d042c46b9d06fafbf48bb50afa4a66d4e2da0cc1
Vagan Pogosyan <vagan2502@gmail.com>
2022-02-18 01:33:56 -0500

    started working on app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("ToDo List");
+
+const pressMe = document.getElementById("press-me");
+const input = document.getElementById("input");
+const containerDiv = document.querySelector(".list");
+
+function appendP() {
+  if (input.value == "" || input.value == " ") {
+    input.value = "";
+    return;
+  }
+  const p = document.createElement("p");
+  p.innerText = input.value;
+  containerDiv.prepend(p);
+  input.value = "";
+}
+
+pressMe.addEventListener("click", appendP);
+
+input.onkeydown = (event) => {
+  if (event.key === "Enter") appendP();
+  return;
+};
