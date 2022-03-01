4afb15f8a076e47d509170a10742af5a1c66991d
Vagan Pogosyan <vagan2502@gmail.com>
2022-02-28 23:36:22 -0500

    appending todo items to DOM and local storage

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// const { stringify } = require("nodemon/lib/utils");
+
 const addButton = document.getElementById("add-button");
 const input = document.getElementById("input");
 const listTodo = document.querySelector(".list-todo");
@@ -5,7 +7,9 @@ const listDone = document.querySelector(".list-done");
 const iconDelete = document.createAttribute("ion-icon");
 const container = document.querySelector(".container");
 
-localStorage.clear();
+// localStorage.clear();
+// let string = "";
+// localStorage.setItem("html", string);
 
 const todoItems = [];
 
@@ -17,23 +21,59 @@ function addItem(text) {
 
   console.log(item);
   todoItems.unshift(item);
+  appendToDOM(item);
+  console.log(todoItems);
+
+  input.value = "";
 }
 
-addItem("go to gym");
-addItem("go to store");
-addItem("go to fred");
-
-function renderTodoItems() {
-  todoItems.forEach((item) => {
-    const element = document.createElement("p");
-    element.className = "list-item";
-    element.innerText = item.text;
-    listTodo.append(element);
-  });
+function addToLocalStorage(item) {
+  localStorage.setItem("html", JSON.stringify(todoItems));
 }
-renderTodoItems();
+
+function appendToDOM(item) {
+  const element = document.createElement("p");
+  element.className = "list-item";
+  element.setAttribute("id", item.id);
+  element.innerText = item.text;
+  listTodo.prepend(element);
+
+  input.value = "";
+}
+
+function renderToDo() {
+  const list = localStorage.getItem("html");
+  if (list) {
+    const todoItems = JSON.parse(list);
+    console.log(todoItems);
+  }
+}
+renderToDo();
+
+addButton.addEventListener("click", () => {
+  addItem(input.value);
+});
+
+input.onkeydown = (event) => {
+  if (event.key === "Enter") addItem(input.value);
+};
+
+// addItem("go to gym");
+// addItem("go to store");
+// addItem("go to fred");
+
+// function renderTodoItems() {
+//   todoItems.forEach((item) => {
+//     const element = document.createElement("p");
+//     element.className = "list-item";
+//     element.innerText = item.text;
+//     listTodo.append(element);
+//   });
+// }
+
+// renderTodoItems();
 // addItem("go to dunny");
-console.log(todoItems);
+// console.log(todoItems);
 
 // function addItem(element, text) {
 //   const htmlTodo = `
